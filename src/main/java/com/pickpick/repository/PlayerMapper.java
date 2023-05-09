336b3ec74ff00e2cd7d1bc7b04d6d25b9c56e0e1
jhlee9462 <jhlee9462@gmail.com>
2023-05-09 19:17:34 +0900

    중간저장

--- src/main/java/com/pickpick/repository/PlayerMapper.java
+++ src/main/java/com/pickpick/repository/PlayerMapper.java
@@ -5,13 +5,15 @@ import com.pickpick.dto.player.PlayerSelectRequestDTO;
 import com.pickpick.entity.Player;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface PlayerMapper {
 
     // 선수 등록
     boolean registerPlayer(Player player);
 
-    // 플레이어 카운트 횟수 업데이트
+    // 선수 카운트 횟수 업데이트
     boolean updatePlayer(PlayerSelectRequestDTO dto);
 
     // 선수 삭제
@@ -19,4 +21,12 @@ public interface PlayerMapper {
 
     // 선수 수정
     boolean modifyPlayer(PlayerModifyRequestDTO dto);
+
+    // 전체 선수 목록 조회
+    List<Player> findAll();
+
+    // 특정 선수 조회
+    Player findOne(int playerId);
+
+
 }
