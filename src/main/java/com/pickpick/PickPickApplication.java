e9ef055364decc64f747a72da2ca4eb793dd7039
jhlee9462 <jhlee9462@gmail.com>
2023-05-09 16:31:45 +0900

    base configuration

--- src/main/java/com/pickpick/PickPickApplication.java
+++ src/main/java/com/pickpick/PickPickApplication.java
@@ -1,13 +1,13 @@
-package com.example.worldcup;
+package com.pickpick;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WorldCupApplication {
+public class PickPickApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WorldCupApplication.class, args);
+        SpringApplication.run(PickPickApplication.class, args);
     }
 
 }
