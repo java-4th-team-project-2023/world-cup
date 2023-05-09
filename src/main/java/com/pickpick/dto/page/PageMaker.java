a0d2edd92f90f16c7a4f9046041b23672e370bc0
oslob99 <jkh991116@naver.com>
2023-05-09 20:43:23 +0900

    #5/9 20:42 account,reply (Entity,xml) 생성

---
+++ src/main/java/com/pickpick/dto/page/PageMaker.java
@@ -0,0 +1,37 @@
+package com.pickpick.dto.page;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter @ToString
+public class PageMaker {
+
+    private static final int PAGE_COUNT = 5;
+    // 화면 렌더링시 페이지의 시작값과 끝값
+    private int begin, end, finalPage;
+
+    // 이전, 다음 버튼 활성화 여부
+    private boolean prev, next;
+
+    // 현재 요청 페이지 정보
+    private Page page;
+
+    // 총 게시물 수
+    private int totalCount;
+
+
+    public PageMaker(Page page, int totalCount) {
+        this.page = page;
+        this.totalCount = totalCount;
+        makePageInfo();
+    }
+
+    private void makePageInfo(){
+        this.end = (int) Math.ceil(page.getPageNo() / (double) PAGE_COUNT ) * PAGE_COUNT;
+        this.begin = this.end - PAGE_COUNT + 1;
+        this.finalPage = (int) Math.ceil((double)totalCount / page.getAmount());
+        if (this.finalPage < this.end) this.end = this.finalPage;
+        this.prev = begin > 1;
+        this.next = end<this.finalPage;
+    }
+}
---
+++ src/main/java/com/pickpick/repository/ReplyMapper.java
@@ -0,0 +1,30 @@
+package com.pickpick.repository;
+
+import com.pickpick.dto.page.Page;
+import com.pickpick.entity.Reply;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface ReplyMapper {
+
+    // 댓글 등록
+    boolean save(Reply reply);
+
+    // 댓글 수정
+    boolean modify(Reply reply);
+
+    // 댓글 삭제
+    boolean deleteOne(int replyNo);
+
+    // 댓글 목록 조회
+    List<Reply> findAll(
+            @Param("gId") int gameId,
+            @Param("page")Page page
+            );
+
+    // 총 댓글 수 조회
+    int count(int gameId);
+}
