fdc18b85f16bd17a9588f9c97e0e290003419a52
jhlee9462 <jhlee9462@gmail.com>
2023-05-22 12:32:04 +0900

    중간저장

--- src/main/resources/static/assets/js/start.js
+++ src/main/resources/static/assets/js/start.js
@@ -39,87 +39,88 @@ async function selectRound() {
     }
 
 // PlayerController 로 데이터 전달
-async function sendRoundToPlayerController(round) {
-    fetch(`/api/v1/players/${document.getElementById('game').dataset.gameId}/num/${round}`,
-        {
-            method: 'POST'
-        })
-        .then(res => res.json())
-        .then(
-            playingGameId => {
-                fetch(`/api/v1/plays/${playingGameId}`)
-                    .then(res => res.json())
-                    .then(({totalRound, currentRound, randomTwoPlayers}) => {
-                        renderPlayers(randomTwoPlayers);
-                        renderRoundInfo(currentRound);
-                        document.getElementById('game').dataset.playingGameId = playingGameId;
-                    });
-            }
-        );
-}
+    async function sendRoundToPlayerController(round) {
+        fetch(`/api/v1/players/${document.getElementById('game').dataset.gameId}/num/${round}`,
+            {
+                method: 'POST'
+            })
+            .then(res => res.json())
+            .then(
+                playingGameId => {
+                    fetch(`/api/v1/plays/${playingGameId}`)
+                        .then(res => res.json())
+                        .then(({totalRound, currentRound, randomTwoPlayers}) => {
+                            renderPlayers(randomTwoPlayers);
+                            renderRoundInfo(currentRound);
+                            document.getElementById('game').dataset.playingGameId = playingGameId;
+                        });
+                }
+            );
+    }
 
 
 // 대결 player 선택하고 게임 다시시작
-function selectOne() {
-    const $game = document.getElementById('game');
+    function selectOne() {
+        const $game = document.getElementById('game');
 
-    $game.onclick = e => {
-        console.log(e.target.dataset.playerId);
-        e.target.classList.add('bigger');
+        $game.onclick = e => {
+            console.log(e.target.dataset.playerId);
+            e.target.classList.add('bigger');
 
-        setTimeout(() => {
-            e.target.classList.remove('bigger');
+            setTimeout(() => {
+                e.target.classList.remove('bigger');
 
-            $game.textContent = '';
-            fetch(`/api/v1/plays/${$game.dataset.playingGameId}/${winnerId}/${loserId}`,
+                $game.textContent = '';
+                fetch(`/api/v1/plays/${$game.dataset.playingGameId}/${winnerId}/${loserId}`,
+                    {
+                        method: 'PUT'
+                    })
+                    .then(res => res.json())
+                    .then(
+                        ({totalRound, currentRound, randomTwoPlayers, gameId}) => {
+
+                            if (currentRound === 0) {
+                                window.location.href = `/rank/winner?gameId=${gameId}&playerId=${winnerId}&round=${totalRound}`;
+                            }
+
+                            renderRoundInfo(currentRound);
+                            renderPlayers(randomTwoPlayers);
+                        }
+                    );
+            }, 2000);
+
+        }
+    }
+
+// 모달 확인 버튼 클릭시 게임 시작, 다음 게임 시작
+    function startGame() {
+        const arr = ['left', 'right'];
+        for (let i = 0; i < 2; i++) {
+            let $section = document.createElement('section')
+            $section.setAttribute('class', arr[i]);
+            $section.setAttribute('data-player-id', i);
+            document.getElementById('game').appendChild($section);
+            console.log($section);
+        }
+
+
+    }
+
+// 리셋 버튼 등록 함수
+    function addResetEvent() {
+        document.getElementById("resetBtn").onclick = e => {
+            const $game = document.getElementById('game');
+            fetch(`/api/v1/plays/${$game.dataset.playingGameId}`,
                 {
                     method: 'PUT'
                 })
-                .then(res => res.json())
-                .then(
-                    ({totalRound, currentRound, randomTwoPlayers, gameId}) => {
+                .then(res => {
 
-                        if (currentRound === 0) {
-                            window.location.href = `/rank/winner?gameId=${gameId}&playerId=${winnerId}&round=${totalRound}`;
-                        }
+                    $game.textContent = '';
 
-                        renderRoundInfo(currentRound);
-                        renderPlayers(randomTwoPlayers);
-                    }
-                );
-        }, 2000);
-
-    }
-}
-
-// 모달 확인 버튼 클릭시 게임 시작, 다음 게임 시작
-function startGame() {
-    const arr = ['left', 'right'];
-    for (let i = 0; i < 2; i++) {
-        let $section = document.createElement('section')
-        $section.setAttribute('class', arr[i]);
-        $section.setAttribute('data-player-id', i);
-        document.getElementById('game').appendChild($section);
-        console.log($section);
-    }
-
-
-}
-
-// 리셋 버튼 등록 함수
-function addResetEvent() {
-    document.getElementById("resetBtn").onclick = e => {
-        const $game = document.getElementById('game');
-        fetch(`/api/v1/plays/${$game.dataset.playingGameId}`,
-            {
-                method: 'PUT'
-            })
-            .then(res => {
-
-                $game.textContent = '';
-
-                return res.json();
-            })
-            .then(({randomTwoPlayers}) => renderPlayers(randomTwoPlayers));
+                    return res.json();
+                })
+                .then(({randomTwoPlayers}) => renderPlayers(randomTwoPlayers));
+        }
     }
 }
