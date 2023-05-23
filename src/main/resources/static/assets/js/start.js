ea2aa06f232ad2c2c8b5246b69f3b1c8be95c8a9
jhlee9462 <jhlee9462@gmail.com>
2023-05-23 19:32:47 +0900

    jpeg 이미지 나오게 + 게임스타트 화면 아래 여백 제거

--- src/main/resources/static/assets/js/start.js
+++ src/main/resources/static/assets/js/start.js
@@ -138,10 +138,12 @@ function renderPlayers(randomTwoPlayers) {
     for (let i = 0; i < 2; i++) {
         const {playerId, playerImgPath, playerName} = randomTwoPlayers[i];
 
+        console.log(playerImgPath);
+
         const $section = document.createElement('section');
         $section.setAttribute('class', arr[i]);
         $section.setAttribute('data-player-id', playerId);
-        $section.style.backgroundImage = `url(${playerImgPath})`;
+        $section.style.backgroundImage = `url('${playerImgPath}')`;
         $section.textContent = playerName;
         document.getElementById('game').appendChild($section);
         // console.log($section);
